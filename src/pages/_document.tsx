1cb08d0293bf23f49b8883108f47d57d030e7a7d
Sagar S Sutar <sutarsagarsanjay@gmail.com>
2024-11-04 17:41:30 +0530

    Formatting changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,18 @@
-/** 
+/**
  * @brief Initial response page that is rendered by & sent from the server.
  * @note A required file for Next.js
  * @refer https://nextjs.org/docs/pages/building-your-application/routing/custom-document
-*/
-import { Html, Head, Main, NextScript } from 'next/document'
- 
+ */
+import { Html, Head, Main, NextScript } from 'next/document';
+
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,25 @@
  * @notes
  * - This page gets automatically detected by Next.js development server. * -
  */
-import React, { FC } from 'react'
-import Form from './form'
-import MovingDot from './movingDot'
-import CollapseBox from './collapseBox'
+import React, { FC } from 'react';
+import Form from './form';
+import MovingDot from './movingDot';
+import CollapseBox from './collapseBox';
+import { Layout } from 'antd';
+
+const { Sider, Header, Footer, Content } = Layout;
 
 const Root: FC = () => (
     <>
-        <CollapseBox />
-        {/* <MovingDot /> */}
-        <Form />
+        <Sider> Sider</Sider>
+        <Layout>
+            <Header> Header </Header>
+            <CollapseBox />
+            {/* <MovingDot /> */}
+            <Form />
+            <Footer> Footer </Footer>
+        </Layout>
     </>
-)
+);
 
-export default Root
+export default Root;
