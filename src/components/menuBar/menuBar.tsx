f1711a3720f0e5a8505b1ba7935619f68b88f904
Sagar S Sutar <sutarsagarsanjay@gmail.com>
2024-12-29 06:35:51 +0530

    Misc. fixes.

--- src/components/menuBar/menuBar.tsx
+++ src/components/menuBar/menuBar.tsx
@@ -9,7 +9,7 @@ import TaskContainer from '@/sections/Managing-State/tasks/taskContainer';
 import HeaderContainer from '@/sections/Managing-State/nestedHeader/nestedHeaderContainer';
 import VideoContainer from '@/sections/Escape-Hatches/video/videoContainer';
 import Home from '@/sections/Home/home';
-import EffectsContainer from '@/sections/Escape-Hatches/Misusing-Effects';
+import ProductContainer from '@/sections/Escape-Hatches/productContainer/productContainer';
 
 type MenuBarProp = {
     width: string;
@@ -19,7 +19,7 @@ type MenuBarProp = {
 const MenuBar: FC<MenuBarProp> = ({ width, onSelect }) => {
     // Menu Items.
     type MenuItem = {
-        key: string;
+        key: number;
         label: string;
         component?: React.ReactNode;
         header?: boolean;
@@ -27,63 +27,63 @@ const MenuBar: FC<MenuBarProp> = ({ width, onSelect }) => {
 
     const menuItems: MenuItem[] = [
         {
-            key: '0',
+            key: 0,
             label: 'Home',
             component: <Home />,
         },
         {
-            key: '1',
+            key: 1,
             label: 'Managing State',
             header: true,
         },
         {
-            key: '2',
+            key: 2,
             label: 'Reacting to Input with State & Choosing the State Structure.',
             component: <Form />,
         },
         {
-            key: '3',
+            key: 3,
             label: 'Sharing State Between Components',
             component: <CollapseBox />,
         },
         {
-            key: '4',
+            key: 4,
             label: 'Preserving and Resetting State',
             component: <CounterContainer />,
         },
 
         {
-            key: '5',
+            key: 5,
             label: 'Extracting State Logic into a Reducer & Scaling Up with Reducer and Context',
             component: <TaskContainer />,
         },
         {
-            key: '6',
+            key: 6,
             label: 'Passing Data Deeply with Context',
             component: <HeaderContainer />,
         },
         {
-            key: '7',
+            key: 7,
             label: 'Escape Hatches',
             header: true,
         },
         {
-            key: '8',
+            key: 8,
             label: 'Synchronizing with Effects',
             component: <VideoContainer />,
         },
         {
-            key: '9',
+            key: 9,
             label: 'You might not need Effects',
-            component: <EffectsContainer />,
+            component: <ProductContainer />,
         },
         {
-            key: '10',
+            key: 10,
             label: 'Miscellaneous',
             header: true,
         },
         {
-            key: '11',
+            key: 11,
             label: 'Maps',
             component: <Maps />,
         },
@@ -101,66 +101,64 @@ const MenuBar: FC<MenuBarProp> = ({ width, onSelect }) => {
     }, []);
 
     return (
-        <div>
-            <Drawer
-                destroyOnClose
-                autoFocus={false}
-                open={drawerOpen}
-                placement="left"
-                mask={false}
-                maskClosable={false}
-                getContainer={false}
-                styles={{
-                    // Wrapper of menu body
-                    wrapper: {
-                        width: width,
-                        boxShadow: 'none',
-                        transition: 'none',
-                        transform: 'none',
-                    },
-                    // Menu body
-                    body: {
-                        backgroundColor: '#111827 ',
-                        padding: '0px',
-                    },
-                    header: { display: 'none' },
-                }}
-            >
-                <List
-                    dataSource={menuItems}
-                    renderItem={(menuItem) => (
-                        <List.Item
-                            onClick={() => {
-                                if (!menuItem.header) {
-                                    onSelect(menuItem.component);
-                                    setActiveMenuItemKey(menuItem.key);
-                                }
-                            }}
-                            key={menuItem.key}
-                            className={` 
+        <Drawer
+            destroyOnClose
+            autoFocus={false}
+            open={drawerOpen}
+            placement="left"
+            mask={false}
+            maskClosable={false}
+            getContainer={false}
+            styles={{
+                // Wrapper of menu body
+                wrapper: {
+                    width: width,
+                    boxShadow: 'none',
+                    transition: 'none',
+                    transform: 'none',
+                },
+                // Menu body
+                body: {
+                    backgroundColor: '#111827 ',
+                    padding: '0px',
+                },
+                header: { display: 'none' },
+            }}
+        >
+            <List
+                dataSource={menuItems}
+                renderItem={(menuItem) => (
+                    <List.Item
+                        onClick={() => {
+                            if (!menuItem.header) {
+                                onSelect(menuItem.component);
+                                setActiveMenuItemKey(menuItem.key);
+                            }
+                        }}
+                        key={menuItem.key}
+                        className={` 
                                 ${
                                     menuItem.key == activeMenuItemKey // Active Item is of different color.
                                         ? 'bg-gray-800 rounded-r-xl'
                                         : ''
                                 }
                             `}
-                        >
-                            <span
-                                className={`px-3 cursor-default text-white 
+                    >
+                        <span
+                            className={`px-3 cursor-default text-white 
                                     ${
                                         menuItem.header
                                             ? 'font-bold  text-slate-400 text-base'
                                             : 'font-normal text-slate-100 pl-5'
                                     }
                                 `}
-                            >
-                                {menuItem.label}
-                            </span>
-                        </List.Item>
-                    )}
-                />
-            </Drawer>
-        </div>
+                        >
+                            {menuItem.label}
+                        </span>
+                    </List.Item>
+                )}
+            />
+        </Drawer>
     );
 };
 
