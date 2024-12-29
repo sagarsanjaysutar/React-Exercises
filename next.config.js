22865af6f57707dff2d452bd414bfa1368a2e623
Sagar S Sutar <sutarsagarsanjay@gmail.com>
2024-12-29 06:34:28 +0530

    Added custom SVG

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
     transpilePackages: ['rc-util', 'rc-pagination', 'rc-picker', '@ant-design/icons-svg'],
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
 }
